b4194df081fa126f938772d6d208e55282871cd8
vishusharmaX <sharmavishwajeet04@gmail.com>
2026-04-07 17:57:37 +0530

    backend updates

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         unique: [ true, "username already taken" ],
-        required: truee,
+        required: true,
     },
 
     email: {
